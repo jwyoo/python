c6282200f473a1a5f4aacfb5eb35538b7176e84b
jwyoo <jwyoo@163.com>
2018-05-16 11:33:19 +0800

    add County Models

--- models.py
+++ models.py
@@ -3,4 +3,6 @@ from django.db import models
 class UserProfile(models.Model):
     name = models.CharField(max_lenth=32)
 
-
+class Country(models.Model):
+    name = models.CharField(max_lenth=32)
+    code = models.SmallIntger()
